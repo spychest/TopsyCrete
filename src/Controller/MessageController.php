fb036cfe437445bcbe2f3221ce77894c9f40a979
spychest <spych3st@gmail.com>
2024-05-01 21:13:24 +0200

    added a protection to access message and fashbag to warn visitor in case of error and success

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -15,10 +15,31 @@ class MessageController extends AbstractController
     public function __construct(
         private readonly MessageFactory $messageFactory,
         private readonly MessageService $messageService,
+        private readonly string $messagePassword
     ) {
     }
 
-    #[Route('/message', name: 'sendMessage')]
+    #[Route('/message', name: 'showMessage')]
+    public function message(Request $request): RedirectResponse|Response
+    {
+        if(!$request->request->get('password')) {
+            $this->addFlash('error', 'Vous devez indiquer un mot de passe');
+            return $this->redirectToRoute('index');
+        }
+
+        $givenPassword = $request->request->get('password');
+
+        if($givenPassword !== $this->messagePassword) {
+            $this->addFlash('error', 'Mauvais mot de passe');
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('message/showMessage.html.twig', [
+        'messages' => $this->messageService->getMessages()
+        ]);
+    }
+
+    #[Route('/sendMessage', name: 'sendMessage')]
     public function sendMessage(): Response
     {
         return $this->render('message/index.html.twig', [
@@ -35,7 +56,7 @@ class MessageController extends AbstractController
             $this->addFlash('error', $exception->getMessage());
             return $this->redirectToRoute('sendMessage');
         }
-
+        $this->addFlash('success', 'Votre message à bien été ajouté. Merci !');
         return $this->redirectToRoute('index');
     }
 }
