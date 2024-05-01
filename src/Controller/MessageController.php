d38c9094061f0f0fb83a89d0a0aee0a2827bc70b
spychest <spych3st@gmail.com>
2024-05-01 20:41:33 +0200

    use new form

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Service\Attribute\Required;
 
 class MessageController extends AbstractController
 {
